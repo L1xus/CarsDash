41f3828740c2aa1a61007b870fb1973cc40f5b33
L1xus <omarstellar456@gmail.com>
2024-11-13 23:18:30 +0100

    add multithreading to run two scripts in parallel

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
+import threading
+import queue
 from moteur.scrape import scrape_moteur
 from avito.scrape import scrape_avito
-
 from datetime import datetime
+
+results_queue = queue.Queue()
+
+def scrape_return(scrape_function, *args):
+    result = scrape_function(*args)
+    results_queue.put(result)
 
 start_time = datetime.now()
 
+moteur_thread = threading.Thread(target=scrape_return, args=(scrape_moteur, 5))
+avito_thread = threading.Thread(target=scrape_return, args=(scrape_avito, 5))
 
-moteur_cars = scrape_moteur(5)
-print(moteur_cars)
-avito_cars = scrape_avito(5)
-print(avito_cars)
+moteur_thread.start()
+avito_thread.start()
+
+moteur_thread.join()
+avito_thread.join()
+
+moteur_cars = results_queue.get()
+print("Moteur cars:", moteur_cars)
+
+avito_cars = results_queue.get()
+print("Avito cars:", avito_cars)
 
 end_time = datetime.now()
 print("Duration: {}".format(end_time - start_time))
